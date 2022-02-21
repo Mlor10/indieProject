0a831ae7090361954dcd5aaa250ea98c9a32f667
Mlor10 <mtlor@madisoncollege.edu>
2022-02-21 07:54:59 -0600

    Add test directories and classes

--- src/main/java/project/test/test.java
+++ src/main/java/project/test/test.java
@@ -6,10 +6,6 @@ import org.apache.logging.log4j.Logger;
 public class test {
     private final Logger logger = LogManager.getLogger(this.getClass());
 
-    public static void main(String[] args) {
-        logInfo();
-    }
-
     public void logInfo() {
         logger.info("This is a message from the logger");
     }
---
+++ src/test/java/project/persistence/UserDaoTest.java
@@ -0,0 +1,8 @@
+package project.persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class UserDaoTest {
+}
