fce782e722bf1b94d4980b554ea76515cd5251e0
Mlor10 <mtlor@madisoncollege.edu>
2022-02-21 08:34:12 -0600

    Update SessionFactoryProvider.java, User.java, and UserDao

--- src/main/java/project/entity/User.java
+++ src/main/java/project/entity/User.java
@@ -1,4 +1,122 @@
 package project.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+/**
+ * A class to represent a user
+ */
+@Entity(name = "User")
+@Table(name = "user")
 public class User {
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "user_name")
+    private String userName;
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private int id;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    /**
+     * Instantiates a new user
+     */
+    public User() {
+    }
+
+    /**
+     * Gets user's first name
+     *
+     * @return first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets user's first name
+     *
+     * @param firstName user first name
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Gets user's last name
+     *
+     * @return last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets user's last name
+     *
+     * @param lastName user last name
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Gets user's username
+     *
+     * @return username
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Sets user's username
+     *
+     * @param userName user username
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * Gets user's id
+     *
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets user's id
+     *
+     * @param id user id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets user's date of birth
+     *
+     * @return date of birth
+     */
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    /**
+     * Sets user's date of birth
+     *
+     * @param dateOfBirth user date of birth
+     */
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
--- src/main/java/project/persistence/UserDao.java
+++ src/main/java/project/persistence/UserDao.java
@@ -1,4 +1,35 @@
 package project.persistence;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import project.entity.User;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 public class UserDao {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    /**
+     * Return a list of all users
+     * @return all users
+     */
+    public List<User> getAll() {
+
+        Session session = sessionFactory.openSession();
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        List<User> users = session.createQuery(query).getResultList();
+
+        logger.debug("The list of users " + users);
+        session.close();
+
+        return users;
+    }
 }
