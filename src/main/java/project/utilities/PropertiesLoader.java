25a7cf96ef54660c584a186842a132202dec1ced
Mlor10 <mtlor@madisoncollege.edu>
2022-02-27 14:09:20 -0600

    Update PropertiesLoader.java replaced stacktrace with log4j

--- src/main/java/project/utilities/PropertiesLoader.java
+++ src/main/java/project/utilities/PropertiesLoader.java
@@ -1,19 +1,20 @@
 package project.utilities;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.io.IOException;
 import java.util.Properties;
 
 public interface PropertiesLoader {
+    private final Logger logger = LogManager.getLogger(this.getClass());
     default Properties loadProperties(String propertiesFilePath) {
         Properties properties = new Properties();
         try {
             properties.load (this.getClass().getResourceAsStream(propertiesFilePath));
         } catch (IOException ioe) {
-            System.out.println("Database.loadProperties()...Cannot load the properties file");
-            ioe.printStackTrace();
+            logger.error("Database.loadProperties()...Cannot load the properties file", ioe);
         } catch (Exception e) {
-            System.out.println("Database.loadProperties()..." + e);
-            e.printStackTrace();
+            logger.error("Database.loadProperties()...", e);
         }
         return properties;
     }
