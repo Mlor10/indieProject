471eb6e098e2bd3280956d8e03ba60496138e9fa
Mlor10 <mtlor@madisoncollege.edu>
2022-04-29 15:40:54 -0500

    Update log4j properties file to replace rather than append logs so it doesn't clutter AWS and added a memory swap config for AWS to resolve ec2 instance errors

--- src/main/java/project/service/SearchProfile.java
+++ src/main/java/project/service/SearchProfile.java
@@ -2,21 +2,15 @@ package project.service;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.glassfish.jersey.server.mvc.Viewable;
 import project.entity.User;
 import project.persistence.GenericDao;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
-import java.io.IOException;
-import java.util.List;
+import javax.ws.rs.core.MediaType;
 
 @Path("/profile")
 public class SearchProfile {
@@ -29,26 +23,8 @@ public class SearchProfile {
 
     @GET
     @Path("/{param}")
-    @Produces("text/html")
-    public void getProfile(@Context HttpServletResponse resp,
-                         @Context HttpServletRequest req,
-                         @PathParam("param") String userName) throws ServletException, IOException {
-        boolean obtainedUser = false;
-        String url = null;
-        try {
-            List<User> userList = genericDaoUser.getByPropertyEqual("userName", userName);
-            User userProfile = userList.get(0);
-            req.setAttribute("userProfile", userProfile);
-            obtainedUser = true;
-        } catch (Exception e){
-            logger.error("Exception error while searching for user: ", e);
-        }
-        if (obtainedUser) {
-            url = "userprofile.jsp";
-        } else {
-            url = "error.jsp";
-        }
-        RequestDispatcher dispatcher = req.getRequestDispatcher(url);
-        dispatcher.forward(req, resp);
+    @Produces(MediaType.TEXT_HTML)
+    public Viewable getProfile(@PathParam("param") String userName) {
+        return new Viewable("/userprofile");
     }
 }
